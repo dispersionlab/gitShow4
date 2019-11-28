6fbc4fee677903a382e99eb02de0c0c8d971e865
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2019-11-28 11:09:50 -0500

    reverted to original

--- module4.js
+++ module4.js
@@ -3,36 +3,30 @@ config.bufferSize = 16
 
 var phase = 0
 var phase2 = 0
-
 function process(block) {
-
 	// Knob ranges from -5 to 5 octaves
-	// var pitch = block.knobs[0] * 10 - 5
+	var pitch = block.knobs[0] * 10 - 5
 	// Input follows 1V/oct standard
-	// pitch += block.inputs[0][0]	
-
-	// unit = block.knobs[1]
-	display(block.inputs[0][0])
+	pitch += block.inputs[0][0]		
 
 	// The relationship between 1V/oct pitch and frequency is `freq = 2^pitch`.
 	// Default frequency is middle C (C4) in Hz.
 	// https://vcvrack.com/manual/VoltageStandards.html#pitch-and-frequencies
-	// var freq = 261.6256 * Math.pow(2, pitch)
+	var freq = 261.6256 * Math.pow(2, pitch)
 
-	//display("Freq: " + freq.toFixed(3) + " Hz")
+	display("Freq: " + freq.toFixed(3) + " Hz")
 
 	// Set all samples in output buffer
-	var deltaPhase = config.frameDivider * block.sampleTime * 100
+	var deltaPhase = config.frameDivider * block.sampleTime * freq
 	for (var i = 0; i < block.bufferSize; i++) {
 		// Accumulate phase
 		phase += deltaPhase
 		// Wrap phase around range [0, 1]
-		phase %= block.knobs[0]
+		phase %= 1
 		// Convert phase to sine output
-		block.outputs[0][i] = block.inputs[1][0] * phase * 5
+		block.outputs[0][i] = Math.sin(2 * Math.PI * phase) * 5
 		// Convert phase to cosine output
 		block.outputs[1][i] = Math.cos(2 * Math.PI * phase) * 5
-
 	}
 
 
